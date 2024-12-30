b414c4db56246c2d70b57c9411e513b5a173ec81
hardikt1793 <hardikt1793@gmail.com>
2024-12-30 17:42:44 +0530

    code refactor
    Further code refactor adding proper types, formatting, commenting

--- src/app/banner/banner.component.ts
+++ src/app/banner/banner.component.ts
@@ -1,18 +1,22 @@
-import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-banner',
+  selector: "app-banner",
   standalone: true,
   imports: [CommonModule],
-  templateUrl: './banner.component.html',
-  styleUrl: './banner.component.scss'
+  templateUrl: "./banner.component.html",
+  styleUrl: "./banner.component.scss",
 })
 export class BannerComponent {
-  banner = {
-    bannerTitle: 'CIÊNCIAS\nHUMANAS',
-    buttonText: 'Começar',
-    bannerImage: 'assets/images/img1.png',
-  }
-
+  // banner details
+  banner: {
+    title: string;
+    text: string;
+    image: string;
+  } = {
+    title: "CIÊNCIAS\nHUMANAS",
+    text: "Começar",
+    image: "assets/images/img1.png",
+  };
 }
--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,31 +1,39 @@
-import { Component } from '@angular/core';
-import { FormBuilder, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { Component } from "@angular/core";
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from "@angular/forms";
 
 @Component({
-  selector: 'app-register',
+  selector: "app-register",
   standalone: true,
   imports: [ReactiveFormsModule],
-  templateUrl: './register.component.html',
-  styleUrl: './register.component.scss'
+  templateUrl: "./register.component.html",
+  styleUrl: "./register.component.scss",
 })
 export class RegisterComponent {
-
+  // Register form
   registerForm: FormGroup;
 
   constructor(private fb: FormBuilder) {
     this.registerForm = this.fb.group({
-      name: ['', [Validators.required]],
-      email: ['', [Validators.required, Validators.email]],
-      password: ['', [Validators.required, Validators.minLength(6)]],
+      name: ["", [Validators.required]],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", [Validators.required, Validators.minLength(6)]],
     });
   }
 
-  onSubmit() {
+  /**
+   * Submitting the form.
+   */
+  onSubmit(): void {
     if (this.registerForm.valid) {
-      console.log('Form Submitted', this.registerForm.value);
+      console.log("Form Submitted", this.registerForm.value);
     } else {
       this.registerForm.markAllAsTouched();
-      console.log('Form Invalid');
+      console.log("Form Invalid");
     }
   }
 }
